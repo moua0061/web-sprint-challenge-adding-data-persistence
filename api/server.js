73d04de5ec2031a19e56e35147a6378cbb74fe03
Lia Moua <moua0061@yahoo.com>
2022-01-21 07:50:55 -0500

    completed my server and index and basic router endpoints

--- api/server.js
+++ api/server.js
@@ -1,1 +1,27 @@
-// build your server here and require it from index.js
+const express = require('express');
+//import routers
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
+
+const server = express();
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
+server.use(express.json());
+
+server.use('*/', (req, res) => {
+    res.send(`<h1>hello world</h1>`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+
+module.exports = server;
